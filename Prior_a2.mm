c10caf3f542155baaba15361ee6c481849ae2a5e
mrnobody <mrnobody@nowhere.special>
2017-07-18 20:35:32 -0600

    Added map

---
+++ Prior_a2.mm
@@ -0,0 +1,76 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1499649974626" ID="ID_1855315789" MODIFIED="1499650271668" TEXT="Prior Analytics">
+<font NAME="SansSerif" SIZE="24"/>
+<node CREATED="1499650284435" ID="ID_398197113" MODIFIED="1499650909470" POSITION="right" TEXT="Chapter 1">
+<edge WIDTH="2"/>
+<node CREATED="1499650318316" HGAP="46" ID="ID_1381871717" MODIFIED="1499651109997" TEXT="premiss" VSHIFT="-8">
+<node CREATED="1499650754663" HGAP="25" ID="ID_3386174" MODIFIED="1499651030885" TEXT="defined" VSHIFT="-2">
+<font NAME="SansSerif" SIZE="10"/>
+<node CREATED="1499651035836" ID="ID_890804797" MODIFIED="1499651053848" TEXT="affirms or denies something of something"/>
+</node>
+<node CREATED="1499651133888" ID="ID_1543548279" MODIFIED="1499651198487" TEXT="can be">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="1">dc</font>
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="10"/>
+<node CREATED="1499651194544" ID="ID_1464485421" MODIFIED="1499651224008" TEXT="universal"/>
+<node CREATED="1499651198487" ID="ID_476800045" MODIFIED="1499651230272" TEXT="particular"/>
+<node CREATED="1499651231639" ID="ID_1460687893" MODIFIED="1499651238735" TEXT="indeterminate"/>
+</node>
+<node CREATED="1499651255236" ID="ID_793202469" MODIFIED="1499651311877" TEXT="can be">
+<font NAME="SansSerif" SIZE="10"/>
+<node CREATED="1499651297500" FOLDED="true" ID="ID_1191439763" MODIFIED="1499651375413" TEXT="dialectical">
+<node CREATED="1499651327097" ID="ID_1339014121" MODIFIED="1499651344161" TEXT="a question about a pair of contradictories"/>
+</node>
+<node CREATED="1499651314272" FOLDED="true" ID="ID_1955408926" MODIFIED="1499651372688" TEXT="demonstrative">
+<node CREATED="1499651347542" ID="ID_1941552586" MODIFIED="1499651370289" TEXT="taking of one part of a pair of contradictories"/>
+</node>
+</node>
+</node>
+<node CREATED="1499650802060" HGAP="64" ID="ID_779044419" MODIFIED="1500149736371" TEXT="&apos;about what and of what&apos;:: about demonstration and of demonstrative science" VSHIFT="-186">
+<node CREATED="1500145083133" ID="ID_1285175612" MODIFIED="1500149529651" TEXT="objective genative">
+<node CREATED="1500145219507" ID="ID_473084648" MODIFIED="1500151024401" TEXT="&apos;about what&apos; is an activity &apos;of what&apos;, the more obvious perspective "/>
+<node CREATED="1500147252907" ID="ID_1538652107" MODIFIED="1500147262749" TEXT="def">
+<font NAME="SansSerif" SIZE="10"/>
+<node CREATED="1500147273765" ID="ID_690090388" MODIFIED="1500148042642" TEXT="genative marks a noun as possessing another noun. Genative is objective if one noun is the direct OBJECT of another. ex: &quot;the thing that &apos;caused&apos; the &apos;journey&apos;.&quot;"/>
+</node>
+<node CREATED="1500149530910" ID="ID_1738024451" MODIFIED="1500150269016" TEXT="my take">
+<font NAME="SansSerif" SIZE="10"/>
+<node CREATED="1500150286888" ID="ID_1982779955" MODIFIED="1500151705997" TEXT="to demonstrate is an activity of demonstrative science, so there is no implicit ordering of demonstration CONTAINED in demonstrative science"/>
+</node>
+</node>
+<node CREATED="1500145203550" ID="ID_1133114775" MODIFIED="1500147952114" TEXT="subjective genative">
+<node CREATED="1500145331860" ID="ID_1198003685" MODIFIED="1500145516389" TEXT="&apos;about what&apos; = demonstration"/>
+<node CREATED="1500145519196" ID="ID_1494618693" MODIFIED="1500145650534" TEXT="&apos;of what&apos; = what category demostration falls under">
+<node CREATED="1500145981026" ID="ID_158313484" MODIFIED="1500146013047" TEXT="unlikely case">
+<font NAME="SansSerif" SIZE="10"/>
+<node CREATED="1500145623084" ID="ID_1096398631" MODIFIED="1500145647582" TEXT="a) is a demonstrative science">
+<node CREATED="1500151839065" ID="ID_1955323011" MODIFIED="1500152068328" TEXT="would imply that syllogism is axiomatic in nature which is unlikely because of the lack of logical connectives means that syllogism can not define itself which would imply that aristotle would be wrong that all scientific questions are provable through the syllogism"/>
+</node>
+</node>
+<node CREATED="1500146019625" ID="ID_1978221027" MODIFIED="1500146042311" TEXT="likely what Aristotle thought">
+<font NAME="SansSerif" SIZE="10"/>
+<node CREATED="1500145650534" HGAP="63" ID="ID_1076340294" MODIFIED="1500145874828" TEXT="b) belongs to a generalized demonstrative science" VSHIFT="25"/>
+</node>
+</node>
+<node CREATED="1500147855027" ID="ID_1439345672" MODIFIED="1500147862546" TEXT="def">
+<font NAME="SansSerif" SIZE="10"/>
+<node CREATED="1500147863946" ID="ID_925021856" MODIFIED="1500148885594" TEXT="genative marks a noun as possessing another noun. genative is subjective if one noun is a subject of the other. ex: &apos;&quot;the arrival, &apos;of ceaser&apos;&quot;(who arrived? Ceaser)"/>
+</node>
+<node CREATED="1500150498220" ID="ID_530118643" MODIFIED="1500150506146" TEXT="my take">
+<font NAME="SansSerif" SIZE="10"/>
+<node CREATED="1500150507698" ID="ID_310011655" MODIFIED="1500151808627" TEXT="we speak of demonstration as a thing, and the thing that it is a part of is a demonstrative science or a part of demonstrative science. there is some sort of objectifying process, where &apos;demonstration&apos; is a thing and it is a cog of the machine &apos;demonstrative science&apos;"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
