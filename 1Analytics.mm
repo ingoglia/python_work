ecf903ce1246658c729355fca309a7fa32eb678b
Dominique Ingoglia <ingoglia@keemail.me>
2017-07-21 21:01:20 -0600

    work for july 21

--- 1Analytics.mm
+++ 1Analytics.mm
@@ -1,10 +1,10 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1499649974626" ID="ID_1855315789" MODIFIED="1499650271668" TEXT="Prior Analytics">
+<node CREATED="1499649974626" ID="ID_1855315789" MODIFIED="1500675871897" TEXT="Aristotle&apos;s Logic">
 <font NAME="SansSerif" SIZE="24"/>
-<node CREATED="1499650284435" ID="ID_398197113" MODIFIED="1499650909470" POSITION="right" TEXT="Chapter 1">
+<node CREATED="1499650284435" ID="ID_398197113" MODIFIED="1500675246709" POSITION="right" TEXT="chapter 1">
 <edge WIDTH="2"/>
-<node CREATED="1499650318316" HGAP="46" ID="ID_1381871717" MODIFIED="1499651109997" TEXT="premiss" VSHIFT="-8">
+<node CREATED="1499650318316" FOLDED="true" HGAP="46" ID="ID_1381871717" MODIFIED="1500657345625" TEXT="premiss" VSHIFT="-8">
 <node CREATED="1499650754663" HGAP="25" ID="ID_3386174" MODIFIED="1499651030885" TEXT="defined" VSHIFT="-2">
 <font NAME="SansSerif" SIZE="10"/>
 <node CREATED="1499651035836" ID="ID_890804797" MODIFIED="1499651053848" TEXT="affirms or denies something of something"/>
@@ -27,15 +27,15 @@
 </node>
 <node CREATED="1499651255236" ID="ID_793202469" MODIFIED="1499651311877" TEXT="can be">
 <font NAME="SansSerif" SIZE="10"/>
-<node CREATED="1499651297500" FOLDED="true" ID="ID_1191439763" MODIFIED="1499651375413" TEXT="dialectical">
+<node CREATED="1499651297500" FOLDED="true" ID="ID_1191439763" MODIFIED="1500657343395" TEXT="dialectical">
 <node CREATED="1499651327097" ID="ID_1339014121" MODIFIED="1499651344161" TEXT="a question about a pair of contradictories"/>
 </node>
-<node CREATED="1499651314272" FOLDED="true" ID="ID_1955408926" MODIFIED="1499651372688" TEXT="demonstrative">
+<node CREATED="1499651314272" FOLDED="true" ID="ID_1955408926" MODIFIED="1500657344108" TEXT="demonstrative">
 <node CREATED="1499651347542" ID="ID_1941552586" MODIFIED="1499651370289" TEXT="taking of one part of a pair of contradictories"/>
 </node>
 </node>
 </node>
-<node CREATED="1499650802060" HGAP="64" ID="ID_779044419" MODIFIED="1500149736371" TEXT="&apos;about what and of what&apos;:: about demonstration and of demonstrative science" VSHIFT="-186">
+<node CREATED="1499650802060" HGAP="64" ID="ID_779044419" MODIFIED="1500675260286" TEXT="&apos;about what and of what&apos;:: about demonstration and of demonstrative science" VSHIFT="-186">
 <node CREATED="1500145083133" ID="ID_1285175612" MODIFIED="1500149529651" TEXT="objective genative">
 <node CREATED="1500145219507" ID="ID_473084648" MODIFIED="1500151024401" TEXT="&apos;about what&apos; is an activity &apos;of what&apos;, the more obvious perspective "/>
 <node CREATED="1500147252907" ID="ID_1538652107" MODIFIED="1500147262749" TEXT="def">
@@ -71,16 +71,104 @@
 </node>
 </node>
 </node>
-<node CREATED="1500572266487" ID="ID_1422407627" MODIFIED="1500573082288" TEXT="syllogism">
+<node CREATED="1500572266487" FOLDED="true" ID="ID_1422407627" MODIFIED="1500657513533" TEXT="syllogism">
 <node CREATED="1500572280003" ID="ID_1515795397" MODIFIED="1500572529036" TEXT="def">
 <font NAME="SansSerif" SIZE="10"/>
 <node CREATED="1500572531614" ID="ID_1752116772" MODIFIED="1500572880981" TEXT="a VALID deductive &apos;argument&apos; taking a specific form of 3 lines"/>
 <node CREATED="1500573103214" ID="ID_31915346" MODIFIED="1500573126999" TEXT="the conclusion must be different from the premises"/>
 <node CREATED="1500573355072" ID="ID_65127444" MODIFIED="1500573377781" TEXT="unclear if a conversion is a syllogism"/>
-<node CREATED="1500573082914" ID="ID_392594521" MODIFIED="1500573237605" TEXT="must have more than one premise or be convertable to one with more than one premise or be a petito principii"/>
+<node CREATED="1500573082914" ID="ID_392594521" MODIFIED="1500585368978" TEXT="must have more than one premise or be convertable to one "/>
 <node CREATED="1500572882616" ID="ID_1377787526" MODIFIED="1500572897158" TEXT="to be contrasted with inductive arguments"/>
+<node CREATED="1500585528235" ID="ID_265593696" MODIFIED="1500585560457" TEXT="the conclusion follows from &apos;logical consequence&apos;"/>
+</node>
+<node CREATED="1500655457728" ID="ID_990583322" MODIFIED="1500655468696" TEXT="perfect syllogism">
+<node CREATED="1500655468699" ID="ID_17646004" MODIFIED="1500655486034" TEXT="def">
+<font NAME="SansSerif" SIZE="10"/>
+<node CREATED="1500655487330" ID="ID_286321923" MODIFIED="1500655562768" TEXT="the necessity of the inference is obvious once the the premises are given"/>
 </node>
 </node>
+<node CREATED="1500655751227" ID="ID_1422542286" MODIFIED="1500655759399" TEXT="imperfect syllogism">
+<node CREATED="1500655759401" ID="ID_1167505842" MODIFIED="1500655765633" TEXT="def">
+<font NAME="SansSerif" SIZE="10"/>
+<node CREATED="1500655765635" ID="ID_1959500267" MODIFIED="1500655790700" TEXT="there are premises that are missing to make a conclusion obvious"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1500585178780" ID="ID_1487555968" MODIFIED="1500675073448" POSITION="left" TEXT="vocabulary">
+<node CREATED="1500585188306" ID="ID_1211368390" MODIFIED="1500585211893" TEXT="petitio principii">
+<node CREATED="1500585214099" ID="ID_1513027442" MODIFIED="1500585269517" TEXT="begging the question">
+<node CREATED="1500585297111" ID="ID_1192111027" MODIFIED="1500585312028" TEXT="assume the conclusion of the question"/>
+</node>
+</node>
+<node CREATED="1500652744489" ID="ID_1885206067" MODIFIED="1500658632497" TEXT="antecedent">
+<node CREATED="1500652768992" ID="ID_1023297959" MODIFIED="1500652840390" TEXT="the conditional element in a proposition"/>
+</node>
+<node CREATED="1500652933591" ID="ID_1543129963" MODIFIED="1500652937296" TEXT="conditional">
+<node CREATED="1500652939272" ID="ID_822387722" MODIFIED="1500652976202" TEXT="asserting that the existence or occurrence of one thing or event depends on the existence or occurrence of another thing or event; hypothetical"/>
+</node>
+<node CREATED="1500653007992" ID="ID_1028439698" MODIFIED="1500653011444" TEXT="consequent">
+<node CREATED="1500653013867" ID="ID_1781719880" MODIFIED="1500653050182" TEXT="the second member of a conditional proposition"/>
+</node>
+<node CREATED="1500655573917" ID="ID_269127221" MODIFIED="1500655578747" TEXT="premise">
+<node CREATED="1500655581185" ID="ID_256478589" MODIFIED="1500655678747" TEXT="a premise is a step in an argument put forward toward a conclusion, it can not stand alone."/>
+</node>
+<node CREATED="1500655690401" ID="ID_1188300223" MODIFIED="1500655694959" TEXT="proposition">
+<node CREATED="1500655696382" ID="ID_1613998346" MODIFIED="1500655904946" TEXT="the meaning of a given statement(can be either true or false)"/>
+</node>
+<node CREATED="1500655835431" ID="ID_1121063931" MODIFIED="1500655840003" TEXT="predicate">
+<node CREATED="1500655844447" ID="ID_785599092" MODIFIED="1500655947333" TEXT="the meaning of given statement(the truth value depends on the value of one or more variables)"/>
+</node>
+<node CREATED="1500655841316" ID="ID_1590091732" MODIFIED="1500656114056" TEXT="assertoric">
+<node CREATED="1500656115576" ID="ID_1510662301" MODIFIED="1500656145218" TEXT="stating a fact as opposed to expressing an evaluative judgment"/>
+</node>
+<node CREATED="1500658079524" ID="ID_658593667" MODIFIED="1500658087483" TEXT="predicate/subject">
+<node CREATED="1500658087484" ID="ID_1230800885" MODIFIED="1500658116133" TEXT="predicate=what the subject does, subject= what the sentence is about"/>
+</node>
+<node CREATED="1500658670832" ID="ID_1541198098" MODIFIED="1500658683007" TEXT="syllogism">
+<node CREATED="1500658684838" ID="ID_1820748123" MODIFIED="1500658723039" TEXT="consists of 2 premisses and a conclusion, containing 3 different terms"/>
+</node>
+<node CREATED="1500658820658" ID="ID_82703199" MODIFIED="1500658822601" TEXT="moods">
+<node CREATED="1500658915383" ID="ID_13495834" MODIFIED="1500658939000" TEXT="the schematic organization of the predicate and subject"/>
+</node>
+<node CREATED="1500658962558" ID="ID_1328785677" MODIFIED="1500658967023" TEXT="middle term">
+<node CREATED="1500658969062" ID="ID_1957279791" MODIFIED="1500659002345" TEXT="term that is repeated in the syllogism"/>
+</node>
+<node CREATED="1500659050753" ID="ID_1312226227" MODIFIED="1500659053014" TEXT="figure">
+<node CREATED="1500659129573" ID="ID_1868764161" MODIFIED="1500659153569" TEXT="3 different possible pairing of premises based on the middle term"/>
+</node>
+</node>
+<node CREATED="1500656625249" FOLDED="true" ID="ID_641114585" MODIFIED="1500675087483" POSITION="right" TEXT="commentary">
+<node CREATED="1500656637075" ID="ID_618852508" MODIFIED="1500657074342" TEXT="constant symbols">
+<node CREATED="1500656649752" ID="ID_301845451" MODIFIED="1500656848287" TEXT="a: &apos;belongs to every...&apos;( the &apos;a&apos; in &apos;affirmo&apos;)"/>
+<node CREATED="1500656670251" ID="ID_1855575213" MODIFIED="1500656865365" TEXT="e: &apos;belongs to no...&apos;(the &apos;e&apos; in &apos;nego&apos;)"/>
+<node CREATED="1500656688396" ID="ID_1423695456" MODIFIED="1500656889370" TEXT="i: &apos;belongs to some...&apos;(the &apos;i&apos; in &apos;affirmo&apos;)"/>
+<node CREATED="1500656707432" ID="ID_781482224" MODIFIED="1500656911409" TEXT="o: &apos;does not belong to some...&apos;(the &apos;o&apos; in &apos;nego&apos;)"/>
+</node>
+<node CREATED="1500657050068" ID="ID_1506249697" MODIFIED="1500657143232" TEXT="variables">
+<node CREATED="1500657145687" ID="ID_1213225930" MODIFIED="1500657992697" TEXT="A,B,C,D"/>
+</node>
+<node CREATED="1500658179625" ID="ID_1207094204" MODIFIED="1500658186453" TEXT="contradictories">
+<node CREATED="1500658187734" ID="ID_746002446" MODIFIED="1500658268651" TEXT="AaB and AoB ( A belongs to every B is contradictory to A does not belong to some B"/>
+<node CREATED="1500658279443" ID="ID_1714419305" MODIFIED="1500658446202" TEXT="AeB and AiB ( A belongs to no B is c to A belongs to some B)"/>
+</node>
+<node CREATED="1500658728316" ID="ID_1676827147" MODIFIED="1500659182433" TEXT="figures">
+<node CREATED="1500659183721" ID="ID_1809504142" MODIFIED="1500659224124" TEXT="First Figure: AB, BC: the middle term is subject in one premise, predicate in the other"/>
+<node CREATED="1500659226907" ID="ID_1513224261" MODIFIED="1500659260519" TEXT="Second Figure: BA, BC: the middle term is the predicate in both premisses"/>
+<node CREATED="1500659263438" ID="ID_1677658665" MODIFIED="1500659292541" TEXT="Third Figure: AB, CB: the middle term is the subject in both premisses"/>
+</node>
+<node CREATED="1500659357625" ID="ID_1526446592" MODIFIED="1500659360223" TEXT="terms">
+<node CREATED="1500659361344" ID="ID_931406414" MODIFIED="1500659382289" TEXT="major: the predicate of the conclusion"/>
+<node CREATED="1500659385208" ID="ID_336449281" MODIFIED="1500659396099" TEXT="minor: the subject of the conclusion"/>
+</node>
+</node>
+<node CREATED="1500675065974" ID="ID_1238379206" MODIFIED="1500675070851" POSITION="left" TEXT="questions">
+<node CREATED="1500675084500" ID="ID_654880093" MODIFIED="1500675105361" TEXT="what is a syllogism"/>
+<node CREATED="1500675111678" ID="ID_1062725023" MODIFIED="1500675121909" TEXT="what is aristotles logic"/>
+</node>
+<node CREATED="1500675314054" ID="ID_423272971" MODIFIED="1500675314054" POSITION="right" TEXT=""/>
+<node CREATED="1500675829606" ID="ID_219390790" MODIFIED="1500675838255" POSITION="left" TEXT="historical baggage">
+<node CREATED="1500675842387" ID="ID_1799104452" MODIFIED="1500675842387" TEXT=""/>
 </node>
 </node>
 </map>
