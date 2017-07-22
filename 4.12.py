9377f05bd62a7f0fee2d6dc2e46ac3f705c0eba9
mrnobody <mrnobody@nowhere.special>
2017-07-22 15:21:53 -0600

    work done on july 22

---
+++ 4.12.py
@@ -0,0 +1,16 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+
+my_foods.append('cannoli')
+friend_foods.append('ice_cream')
+
+for my_food in my_foods[:]:
+    print(my_food.title())
+print("\n")
+for friend_food in friend_foods[:]:
+    print(friend_food.title())
+    
+
+
+
+
---
+++ 4.13.py
@@ -0,0 +1,8 @@
+buffet = ('chicken', 'cat', 'monkey', 'dog', 'rice')
+for snacks in buffet:
+    print(snacks)
+#buffet[1] = 'cow'
+
+new_buffet = ('chicken', 'cow', 'monkey', 'dog', 'noodles')
+for snackles in new_buffet:
+    print(snackles)
