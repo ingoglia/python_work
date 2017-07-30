91cec8d5185410df65253669170b6fbb769edc81
mrnobody <mrnobody@nowhere.special>
2017-07-29 19:46:47 -0600

    July 29th

---
+++ 7.2.py
@@ -0,0 +1,7 @@
+group = input("How many people will be in your dinner group tonight?")
+group = int(group)
+
+if group > 8:
+    print("Well... you're just going to have to wait then!")
+else:
+    print("Right this way.")
--- Study/Aristotelian_Logic.mm
+++ Study/Aristotelian_Logic.mm
@@ -41,8 +41,8 @@
 </node>
 <node CREATED="1501110886951" ID="ID_502325093" MODIFIED="1501110914582" TEXT="takes as a given that one should not be skeptical of ones senses"/>
 </node>
-<node CREATED="1501006741485" ID="ID_882590816" MODIFIED="1501194050500" TEXT="Essentialism and homonymy">
-<node CREATED="1501195473855" ID="ID_579815546" MODIFIED="1501195481808" TEXT="Essentialism">
+<node CREATED="1501006741485" FOLDED="true" ID="ID_882590816" MODIFIED="1501357934524" TEXT="Essentialism and homonymy">
+<node CREATED="1501195473855" FOLDED="true" ID="ID_579815546" MODIFIED="1501352373712" TEXT="Essentialism">
 <node CREATED="1501006773035" ID="ID_1278945336" MODIFIED="1501006928251" TEXT="essentialism is the belief that everything has an &apos;essense&apos; or set of properties that if one of them were to be lost, that entity would no longer exist"/>
 <node CREATED="1501194502301" ID="ID_1489444148" MODIFIED="1501194559796" TEXT="essence = what is unique to being that thing which cannot be removed"/>
 <node CREATED="1501195362980" ID="ID_1220677154" MODIFIED="1501195398443" TEXT="believes that essence of a kind can be discovered and KNOWN by dialectic"/>
@@ -54,12 +54,17 @@
 </node>
 <node CREATED="1501195164123" ID="ID_506841612" MODIFIED="1501195198467" TEXT="a &apos;kind&apos; is a particular of a universal (Alex is a kind of the human)"/>
 </node>
-<node CREATED="1501195505969" ID="ID_631064913" MODIFIED="1501195512399" TEXT="homonymy">
-<node CREATED="1501045620616" ID="ID_545033691" MODIFIED="1501045640758" TEXT="synonyms are the opposite of homonyms"/>
-<node CREATED="1501046016245" ID="ID_914937447" MODIFIED="1501046048876" TEXT="note that a synonym for aristotle is if one usage of a word is the same as another"/>
-<node CREATED="1501045669136" ID="ID_12622020" MODIFIED="1501045698590" TEXT="multivocity = homonymy"/>
-<node CREATED="1501045701775" ID="ID_124525411" MODIFIED="1501045730013" TEXT="univocity = synonym"/>
+<node CREATED="1501195505969" FOLDED="true" ID="ID_631064913" MODIFIED="1501357933893" TEXT="homonymy">
+<node CREATED="1501045620616" ID="ID_545033691" MODIFIED="1501352369764" TEXT="if synonym=univocity fails, then there might still be homonym or multivocity"/>
+<node CREATED="1501352580778" ID="ID_1098855243" MODIFIED="1501352627456" TEXT="this idea of multi and uni vocity is conceptually between the theory of forms and the family resemblance of Wittenstein"/>
+<node CREATED="1501352684257" ID="ID_814651967" MODIFIED="1501352838608" TEXT="&apos;core dependent homonymy&apos; are the foundational properties"/>
+<node CREATED="1501045669136" ID="ID_12622020" MODIFIED="1501349235430" TEXT="multivocity = homonymy=&apos;being spoken in many ways&apos;"/>
+<node CREATED="1501045701775" ID="ID_124525411" MODIFIED="1501349680490" TEXT="univocity = synonym=if a and b have F, and F is used in the same way for both a and b. ">
+<node CREATED="1501349689075" ID="ID_497348481" MODIFIED="1501349781629" TEXT="in some ways this can be thought of as Aristotles revision of the theory of forms where the univocal property is the property that a single category will take on, it is not cross categorical"/>
+<node CREATED="1501350665269" ID="ID_817897790" MODIFIED="1501350781220" TEXT="can be tested with a set of sentences by paraphrasing the word and seeing if the meaning is the same"/>
+</node>
 <node CREATED="1501195531433" ID="ID_904913045" MODIFIED="1501198690299" TEXT="Aristotle denies Platonic essentialism, arguing that sameness and invarience are homonyms"/>
+<node CREATED="1501352979615" ID="ID_1431848522" MODIFIED="1501353023717" TEXT="there is not fundamental property of &apos;being&apos; because &apos;not-being&apos; is not a possible property"/>
 </node>
 </node>
 </node>
@@ -76,7 +81,7 @@
 <node CREATED="1500677102809" ID="ID_490065879" MODIFIED="1500677148140" TEXT="Thomas Aquinas makes Aristotle popular again in the early 1200&apos;s"/>
 </node>
 </node>
-<node CREATED="1501006075625" ID="ID_1644908462" MODIFIED="1501193975964" POSITION="left" TEXT="types of reasoning">
+<node CREATED="1501006075625" FOLDED="true" ID="ID_1644908462" MODIFIED="1501357936513" POSITION="left" TEXT="types of reasoning">
 <node CREATED="1501006090240" FOLDED="true" ID="ID_1641350830" MODIFIED="1501194044467" TEXT="dialectical">
 <node CREATED="1501006337248" ID="ID_177637119" MODIFIED="1501111399583" TEXT="must respect inference and logical rules"/>
 <node CREATED="1501111278963" ID="ID_1011637328" MODIFIED="1501111366252" TEXT="can rely soley on endoxa unlike scientific reasoning"/>
@@ -134,6 +139,67 @@
 <node CREATED="1501112671422" ID="ID_341408042" MODIFIED="1501112717224" TEXT="seeks to have a conversion tool which will take any type of syllogism and turn it into a perfect syllogism">
 <node CREATED="1501112797449" ID="ID_607827782" MODIFIED="1501112821762" TEXT="vary quantity and quality of each premise/conclusion"/>
 <node CREATED="1501112994502" ID="ID_835366138" MODIFIED="1501113026932" TEXT="uses metatheorems to form conversion tool"/>
+</node>
+</node>
+<node CREATED="1501371979359" FOLDED="true" ID="ID_931749066" MODIFIED="1501373678014" POSITION="right" TEXT="Categories">
+<node CREATED="1501371990459" FOLDED="true" ID="ID_101894151" MODIFIED="1501373604695" TEXT="list of categories">
+<node CREATED="1501372015417" ID="ID_1718904404" MODIFIED="1501372024006" TEXT="substance">
+<node CREATED="1501372971989" ID="ID_102057008" MODIFIED="1501372978503" TEXT="man, horse"/>
+</node>
+<node CREATED="1501372024872" ID="ID_596282548" MODIFIED="1501372029160" TEXT="quantity">
+<node CREATED="1501373003917" ID="ID_1722957102" MODIFIED="1501373011647" TEXT="two-feet long"/>
+</node>
+<node CREATED="1501372029728" ID="ID_1694850583" MODIFIED="1501372037463" TEXT="relative">
+<node CREATED="1501373018593" ID="ID_1006848195" MODIFIED="1501373030495" TEXT="double, slave"/>
+</node>
+<node CREATED="1501372064263" ID="ID_1864285655" MODIFIED="1501372068983" TEXT="acting upon">
+<node CREATED="1501373059492" ID="ID_1559989495" MODIFIED="1501373067950" TEXT="cutting, burning"/>
+</node>
+<node CREATED="1501372069320" ID="ID_355847931" MODIFIED="1501372076103" TEXT="being affected">
+<node CREATED="1501373071238" ID="ID_1662600588" MODIFIED="1501373080390" TEXT="being cut, being burnt"/>
+</node>
+<node CREATED="1501373142807" ID="ID_1127847620" MODIFIED="1501373146125" TEXT="place">
+<node CREATED="1501373148451" ID="ID_1930397346" MODIFIED="1501373152341" TEXT="in the market"/>
+</node>
+<node CREATED="1501373156102" ID="ID_1333026981" MODIFIED="1501373157485" TEXT="time">
+<node CREATED="1501373161525" ID="ID_1001753062" MODIFIED="1501373168738" TEXT="yesterday, tomorrow"/>
+</node>
+<node CREATED="1501373111391" ID="ID_484371426" MODIFIED="1501373114309" TEXT="quality">
+<node CREATED="1501373117048" ID="ID_144967651" MODIFIED="1501373122781" TEXT="white, grammatical"/>
+</node>
+<node CREATED="1501373083663" ID="ID_1566749431" MODIFIED="1501373093165" TEXT="having">
+<node CREATED="1501373099140" ID="ID_1505117373" MODIFIED="1501373102901" TEXT="has shoes on"/>
+</node>
+<node CREATED="1501373175758" ID="ID_447972974" MODIFIED="1501373178165" TEXT="position">
+<node CREATED="1501373182153" ID="ID_1114918208" MODIFIED="1501373186631" TEXT="lying, sitting"/>
+</node>
+</node>
+<node CREATED="1501372166758" ID="ID_740757753" MODIFIED="1501372200957" TEXT="the categorizations are possibly derived from linguistic organizations"/>
+<node CREATED="1501372110527" ID="ID_863709352" MODIFIED="1501372123958" TEXT="intention is to catagorize different ways of being"/>
+<node CREATED="1501372543788" ID="ID_1196736709" MODIFIED="1501372575850" TEXT="substance is like an object"/>
+<node CREATED="1501372576467" ID="ID_548122049" MODIFIED="1501372595226" TEXT="quality is like a verb"/>
+<node CREATED="1501372600507" ID="ID_933724966" MODIFIED="1501372722030" TEXT="the essences are the basic concepts that when combined will create truthvalue statements. They are like terms"/>
+<node CREATED="1501373614487" ID="ID_199484911" MODIFIED="1501373636096" TEXT="all the categories are contingent on substance"/>
+</node>
+<node CREATED="1501373722224" ID="ID_1736069302" MODIFIED="1501373737592" POSITION="left" TEXT="Four Causal explanatory scheme">
+<node CREATED="1501373873032" FOLDED="true" ID="ID_28308706" MODIFIED="1501374196579" TEXT="explaining cause">
+<node CREATED="1501374010404" ID="ID_994203868" MODIFIED="1501374016030" TEXT="material">
+<node CREATED="1501374031157" ID="ID_223536908" MODIFIED="1501374067940" TEXT="that from which something comes or is made of (the bronze of a statue)"/>
+</node>
+<node CREATED="1501374017358" ID="ID_645763103" MODIFIED="1501374018860" TEXT="formal">
+<node CREATED="1501374072019" ID="ID_381461783" MODIFIED="1501374122340" TEXT="the structure or shape of something (it is a statue of &apos;clifford&apos;)"/>
+</node>
+<node CREATED="1501374019471" ID="ID_863414218" MODIFIED="1501374025245" TEXT="efficient">
+<node CREATED="1501374125787" ID="ID_1272344911" MODIFIED="1501374156660" TEXT="the agent responsible for the thing coming into existence (the sculptor)"/>
+</node>
+<node CREATED="1501374026199" ID="ID_11384093" MODIFIED="1501374027547" TEXT="final">
+<node CREATED="1501374162369" ID="ID_398324567" MODIFIED="1501374193722" TEXT="what the purpose or goal of the things is. (the purppose for creating the statue)"/>
+</node>
+</node>
+<node CREATED="1501374203125" ID="ID_463163954" MODIFIED="1501374209187" TEXT="can be found in the physics"/>
+<node CREATED="1501374237277" ID="ID_426450729" MODIFIED="1501374256114" TEXT="necessary and sufficient">
+<node CREATED="1501374264444" ID="ID_1699304249" MODIFIED="1501374286522" TEXT="citing all 4 causes is necesary for adequacy in explanation"/>
+<node CREATED="1501374289913" ID="ID_1693605436" MODIFIED="1501374310346" TEXT="the four causes are sufficient for adequacy in explanation"/>
 </node>
 </node>
 </node>
