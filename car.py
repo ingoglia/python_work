289ff9e7a01890fa299f088b14a2c84cb4ed8021
Dominique Ingoglia <ingoglia@keemail.me>
2017-08-08 13:18:02 -0600

    8/8

--- car.py
+++ car.py
@@ -1,3 +1,5 @@
+"""A class that can be used to represent gas and electric cars."""
+
 class Car():
     """A simple attempt to represent a car."""
 
@@ -31,15 +33,12 @@ class Car():
         """Add the given amount to the odometer reading."""
         self.odometer_reading += miles
             
-#my_new_car = Car('audi', 'a4', 2016)
-#print(my_new_car.get_descriptive_name())
-#my_new_car.update_odometer(23)
-#my_new_car.read_odometer()
 
-my_used_car = Car('subaru', 'outback', 2013)
-print(my_used_car.get_descriptive_name())
 
-my_used_car.update_odometer(23500)
-my_used_car.read_odometer()
-my_used_car.increment_odometer(100)
-my_used_car.read_odometer()
+#my_used_car = Car('subaru', 'outback', 2013)
+#print(my_used_car.get_descriptive_name())
+
+#my_used_car.update_odometer(23500)
+#my_used_car.read_odometer()
+#my_used_car.increment_odometer(100)
+#my_used_car.read_odometer()
